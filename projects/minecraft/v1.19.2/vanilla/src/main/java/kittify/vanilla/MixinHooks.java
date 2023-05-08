ea02c34b2ddc72aea3cc6c91abaec73541a1fd93
BlueAgent <BlueAgent@users.noreply.github.com>
2023-05-08 17:14:36 +0800

    Remove placeholder as it doesn't work

--- projects/minecraft/v1.19.2/vanilla/src/main/java/kittify/vanilla/MixinHooks.java
+++ projects/minecraft/v1.19.2/vanilla/src/main/java/kittify/vanilla/MixinHooks.java
@@ -1,9 +1,7 @@
 package kittify.vanilla;
 
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.TamableAnimal;
 
 public class MixinHooks {
@@ -24,13 +22,7 @@ public class MixinHooks {
         }
 
         if (!simulate) {
-            // TODO: Prevent tamable mob from attacking / dealing damage and being targeted / dealing damage instead of just resetting it here.
-            tamableAnimal.setTarget(null);
-
-            Entity source = damageSource.getEntity();
-            if (source instanceof Mob sourceMob) {
-                sourceMob.setTarget(tamableAnimal.getOwner());
-            }
+            // TODO: Prevent tamable mob from attacking / dealing damage and being targeted / dealing damage.
         }
 
         return true;
